77487c97eb439b24dcdb256e41b1f8425c6746aa
clap104 <claphh@gmail.com>
2017-09-25 01:41:49 -0500

    add libraries

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
-
-
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+import os
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -13,5 +16,8 @@ def hello_world():
 	return 'Hello World!'
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+	port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
