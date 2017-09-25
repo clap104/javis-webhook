cd18587083dfd63233a4fcdbdc782570c9b7116c
clap104 <claphh@gmail.com>
2017-09-25 07:21:16 -0500

    update app.py

--- app.py
+++ app.py
@@ -16,8 +16,9 @@ def hello_world():
 	return 'Hello World!'
 
 if __name__ == '__main__':
-	port = int(os.getenv('PORT', 5000))
+	#port = int(os.getenv('PORT', 5000))
 
-    print("Starting app on port %d" % port)
+    #print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run()
+    #debug=False, port=port, host='0.0.0.0'
